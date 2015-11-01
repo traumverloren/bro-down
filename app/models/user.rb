3f609561f2c6e2545e6a7fbb757a0fd88d2793bf
Stephanie Nemeth <traumverloren@gmail.com>
2015-11-01 21:19:07 +0100

    setting up user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+end
